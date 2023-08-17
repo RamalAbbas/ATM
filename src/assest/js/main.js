9135bd26983ea9d171a7c4aa591ab1bc375b8bd8
Ramal Abbassov <ramalabbasov044@gmail.com>
2023-08-17 09:27:48 +0400

    add

--- src/assest/js/main.js
+++ src/assest/js/main.js
@@ -1,58 +1,70 @@
-class Atm {
-    constructor() {
-        this.my_balance = 0;
-
-        this.increment = function(price){
-            this.my_balance += price
-            showBalance.innerHTML = "Balansivizi yenileyin"
-        }
-
-        this.decrement = function(price){
-            this.my_balance <= 0 || this.my_balance - price < 0 ? alert("Balansivizi yoxlayin") : this.my_balance -= price
-            showBalance.innerHTML = "Balansivizi yenileyin"
-        }
-
-        this.balance = function(){
-            showBalance.innerHTML = "Balans gosterildi"
-            showErr(true,cur_balance,this.my_balance)
-        }
+const atmData = {
+    my_balance: 0,
+    userName: "",
+    cur_balance: null,
+  
+    increment(price) {
+      this.my_balance += price;
+      showBalance.innerHTML = "Balansivizi yenileyin";
+    },
+  
+    decrement(price) {
+      if (this.my_balance <= 0 || this.my_balance - price < 0) {
+        alert("Balansivizi yoxlayin");
+      } else {
+        this.my_balance -= price;
+      }
+      showBalance.innerHTML = "Balansivizi yenileyin";
+    },
+  
+    balance() {
+      showBalance.innerHTML = "Balans gosterildi";
+      showErr(true, this.cur_balance, this.my_balance);
     }
-}
-
+};
+  
+  
+  
 regestration_form.addEventListener("submit", function (event) {
     event.preventDefault();
     let name = this.name_input.value;
     let age = this.age_input.value;
-
-    if(!(name == "" || age == "")){
-        event.target.style.display = "none"
-        document.querySelector(".noShow").style.display = "flex"
-        userName.innerHTML = `Welcome to ${name}`
-        Atm = new Atm(name, age)
+  
+    if (!(name == "" || age == "")) {
+      event.target.style.display = "none";
+      document.querySelector(".noShow").style.display = "flex";
+      atmData.userName = `${name}`;
+      atmData.cur_balance = document.querySelector("#my_balance");
     }
-    showErr(true, err_place, 'Xahis edirik adivizi veya yasinizi girin')
+    showErr(true, err_place, 'Xahis edirik adivizi veya yasinizi girin');
 });
-
-
-
-addMany.addEventListener('click',function(){
-    let price = +prompt("Balansinizi artiramaq ucun qiymet daxil edin .")
-    price >= 0 ?  Atm.increment(price) : alert("sizin qiymetiviz musbet eded olmalidir")
-})
-
-
-payMany.addEventListener('click',function(){
-    let price = +prompt("Balansizinizda cixacaq meblegi yazin")
-    price >= 0 ?  Atm.decrement(price) : alert("sizin qiymetiviz musbet eded olmalidir")
+  
+addMany.addEventListener('click', function () {
+    let price = +prompt("Balansinizi artiramaq ucun qiymet daxil edin.");
+    if (price >= 0) {
+      atmData.increment(price);
+    } else {
+      alert("sizin qiymetiviz musbet eded olmalidir");
+    }
 });
-
-showBalance.addEventListener("click",function(){
-    Atm.balance()
-})
-
-function showErr(bool, err_place, title){
-    if(bool){
-        err_place.innerHTML = title;
+  
+payMany.addEventListener('click', function () {
+    let price = +prompt("Balansizinizda cixacaq meblegi yazin");
+    if (price >= 0) {
+      atmData.decrement(price);
+    } else {
+      alert("sizin qiymetiviz musbet eded olmalidir");
+    }
+});
+  
+showBalance.addEventListener("click", function () {
+    atmData.balance();
+});
+  
+function showErr(bool, err_place, title) {
+    if (bool) {
+      err_place.innerHTML = title;
     }
     return false;
 }
+  
--- src/assest/js/selector.js
+++ src/assest/js/selector.js
@@ -1,11 +1,6 @@
-// constant vaiables
 const regestration_form = document.querySelector("#regestration_form");
 const addMany = document.querySelector("#add_many");
 const payMany = document.querySelector("#pay_many");
-const showBalance = document.querySelector("#show_balance")
-
-// changed variables
-let err_place = document.querySelector("#err_message")
-let form_body = document.querySelector("#show")
-let userName = document.querySelector("#user_name")
-let cur_balance = document.querySelector("#my_balance")
+const showBalance = document.querySelector("#show_balance");
+  
+let err_place = document.querySelector("#err_message");
